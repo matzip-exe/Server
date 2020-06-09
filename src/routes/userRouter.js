1d6433449f006d35791c2d06da90eea1c0aa644d
LeeDongGeon1996 <secmatth1996@gmail.com>
2020-06-09 04:51:42 +0000

    rm libpd

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const staticUrl = "/user";
 
-router.use((req, res,next)=>{
+router.use(staticUrl, (req, res,next)=>{
     console.log("user router");
     console.log("origin URL : " + req.originalUrl);
     console.log("origin URL : " + req.baseUrl);
     res.end();
 });
+
+
 
 module.exports = router;
